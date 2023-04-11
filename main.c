d88fe0eb2ea7b57f7c1f57a1aeb81c1152d83945
Adryan <100885847+adryanhy@users.noreply.github.com>
2023-04-10 21:12:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,62 @@
-
+#
 #include<stdio.h>
 #include<stdlib.h>
 
+void headerEscalonamento(){
+    printf("-------------ESCALONAMENTO-------------\n");
+}
+
 struct processo {
     int pid;
-    int tempo_chegada;
-    int tempo_execucao;
-    int tempo_restante;
+    int tempoChegada;
+    int tempoExecucao;
+    int tempoRestante;
 };
-
+/*ATRIBUIR VALORES NOS PROCESSOS*/
+void preencher_processos(struct processo *processos, int numProcesso) {
+    for (int i = 0; i < numProcesso; i++) {
+        printf("Digite o tempo de chegada e de execucao para o processo %d: ", i + 1);
+        scanf("%d%d", &processos[i].tempoChegada, &processos[i].tempoExecucao);
+        processos[i].pid = i + 1;
+        processos[i].tempoRestante = processos[i].tempoExecucao;
+    }
+}
 int main() {
-    int numProcesso, quantum, tempo_total_espera = 0, tempo_total_retorno = 0;
+    int numProcesso, quantum, TME = 0, TMR = 0;
     struct processo *processos;
-    printf("Digite o número de processos: ");
+    printf("Digite o numero de processos: ");
     scanf("%d", &numProcesso);
     processos = (struct processo*) malloc(numProcesso * sizeof(struct processo));
-    for(int i = 0; i < numProcesso; i++) {
-        printf("Digite o tempo de chegada e de execução para o processo %d: ", i+1);
-        scanf("%d%d", &processos[i].tempo_chegada, &processos[i].tempo_execucao);
-        processos[i].pid = i+1;
-        processos[i].tempo_restante = processos[i].tempo_execucao;
-    }
+    preencher_processos(processos, numProcesso);
     printf("Digite o quantum: ");
     scanf("%d", &quantum);
-    int tempo_atual = 0, concluidos = 0;
-    while(concluidos < numProcesso) {
+    int tempo_atual = 0, processoConcluidos = 0;
+
+    //Logica para validar os processos
+    while(processoConcluidos < numProcesso) {
         for(int i = 0; i < numProcesso; i++) {
-            if(processos[i].tempo_restante > 0) {
-                if(processos[i].tempo_chegada <= tempo_atual) {
-                    if(processos[i].tempo_restante > quantum) {
+            if(processos[i].tempoRestante > 0) {
+                if(processos[i].tempoChegada <= tempo_atual) {
+                    if(processos[i].tempoRestante > quantum) {
                         tempo_atual += quantum;
-                        processos[i].tempo_restante -= quantum;
+                        processos[i].tempoRestante -= quantum;
                     }
                     else {
-                        tempo_atual += processos[i].tempo_restante;
-                        processos[i].tempo_restante = 0;
-                        tempo_total_espera += tempo_atual - processos[i].tempo_chegada - processos[i].tempo_execucao;
-                        tempo_total_retorno += tempo_atual - processos[i].tempo_chegada;
-                        concluidos++;
+                        tempo_atual += processos[i].tempoRestante;
+                        processos[i].tempoRestante = 0;
+                        TME += tempo_atual - processos[i].tempoChegada - processos[i].tempoExecucao;
+                        TMR += tempo_atual - processos[i].tempoChegada;
+                        processoConcluidos++;
                     }
                 }
             }
         }
     }
-    printf("Tempo médio de espera: %f\n", (float) tempo_total_espera / numProcesso);
-    printf("Tempo médio de retorno: %f\n", (float) tempo_total_retorno / numProcesso);
+
+    //O que sera apresentado ao usuario final
+    headerEscalonamento();
+    printf("Tempo medio de espera: %.2f\n", (float) TME / numProcesso);
+    printf("Tempo medio de retorno: %.2f\n", (float) TMR / numProcesso);
     free(processos);
     return 0;
 }
-
